391a023a78c273914e881afaa119c9ce9826f8e2
Minjo Park <consistent.312@gmail.com>
2025-03-10 18:17:51 +0900

    add: 타입 파일 추가 및 기본 타입 정의

---
+++ src/types/Item.ts
@@ -0,0 +1,35 @@
+export type Item = {
+  name: string;
+  description: string;
+  colloq: string;
+  plaintext: string;
+  info: string[];
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    z: number;
+    w: number;
+    h: number;
+  };
+  gold: {
+    base: number;
+    purchasable: boolean;
+    total: number;
+    sell: number;
+  };
+  tags: string[];
+  maps: {
+    11: boolean;
+    12: boolean;
+    21: boolean;
+    22: boolean;
+    30: boolean;
+    33: boolean;
+  };
+  stats: {
+    FlatMovementSpeedMod: number;
+  };
+};
