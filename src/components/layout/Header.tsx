2d7cb27dcd0b7c64cc19d324f522a77cdf7d02ea
Minjo Park <consistent.312@gmail.com>
2025-03-10 23:08:05 +0900

    design: nav css 적용

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,6 @@
 import { PATH } from '@/constants/RouterPath';
 import Link from 'next/link';
+import clsx from 'clsx';
 
 const Header = () => {
   const navList = [
@@ -10,14 +11,23 @@ const Header = () => {
   ];
 
   return (
-    <nav>
-      {navList.map((nav) => {
-        return (
-          <Link key={nav.name} href={nav.href}>
-            {nav.name}
-          </Link>
-        );
-      })}
+    <nav className="w-screen h-[60px] bg-gray-900 text-white flex items-center px-8">
+      <div className="flex gap-3">
+        {navList.map((nav) => {
+          return (
+            <Link
+              key={nav.name}
+              href={nav.href}
+              className={clsx(
+                'rounded-lg px-3 py-1',
+                nav.name !== '홈' && 'hover:bg-gray-700'
+              )}
+            >
+              {nav.name}
+            </Link>
+          );
+        })}
+      </div>
     </nav>
   );
 };
