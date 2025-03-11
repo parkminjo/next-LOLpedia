ba8219e9ac884ac163c475307a15d90d2cb39ae4
Minjo Park <consistent.312@gmail.com>
2025-03-11 17:29:44 +0900

    refactor: ChampionList 컴포넌트로 변경

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,9 +1,7 @@
-import ChampionCard from '@/components/champions/ChampionCard';
+import ChampionList from '@/components/champions/ChampionList';
 import { Text } from '@/components/ui/Text';
 import Champion from '@/types/Champion';
-import { Suspense } from 'react';
 import { fetchChampions } from '../api/fetchData';
-import Loading from '../items/loading';
 
 const Champions = async () => {
   const championList: Champion[] = await fetchChampions();
@@ -16,13 +14,7 @@ const Champions = async () => {
           LOL에서 제공하는 140여 명의 챔피언을 구경해보세요.
         </Text>
       </div>
-      <Suspense fallback={<Loading />}>
-        <div className="grid grid-cols-4 gap-8">
-          {championList.map((champion) => {
-            return <ChampionCard key={champion.key} champion={champion} />;
-          })}
-        </div>
-      </Suspense>
+      <ChampionList championList={championList} />
     </div>
   );
 };
