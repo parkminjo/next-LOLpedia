9e728b650b47fe8ff3d04422e8f392dabd63a8fd
Minjo Park <consistent.312@gmail.com>
2025-03-11 19:35:32 +0900

    chore: url 내용 수정

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -7,7 +7,7 @@ export const URL: Record<string, string> = {
   CHAMPION_IMG_URL:
     'https://ddragon.leagueoflegends.com/cdn/img/champion/splash',
   ITEMS_DATA_URL:
-    'https://ddragon.leagueoflegends.com/cdn/10.6.1/data/ko_KR/item.json',
-  ITEMS_IMG_URL: 'https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item',
+    'https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json',
+  ITEMS_IMG_URL: 'https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item',
   CHAMPIONS_ROTATION_DATA_URL: `https://kr.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${process.env.NEXT_PUBLIC_RIOT_API_KEY}`,
 };
