54673b8ebf0cdb7bdf47bd32ae33cec03d60442f
Minjo Park <consistent.312@gmail.com>
2025-03-11 14:32:51 +0900

    feat: 챔피언 데이터 불러오기

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,5 +1,26 @@
-const Champions = () => {
-  return <div>Champions</div>;
+import ChampionItem from '@/components/champions/ChampionItem';
+import { REVALIDATE_TIME_24_HOURS } from '@/constants/number';
+import { URL } from '@/constants/url';
+import { Champion } from '@/types/Champion';
+
+const Champions = async () => {
+  const response = await fetch(URL.CHAMPIONS_DATA_URL, {
+    next: {
+      revalidate: REVALIDATE_TIME_24_HOURS,
+    },
+  });
+  const { data } = await response.json();
+  const championList: Champion[] = Object.values(data);
+
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="grid grid-cols-4 gap-8">
+        {championList.map((champion) => {
+          return <ChampionItem key={champion.key} champion={champion} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Champions;
