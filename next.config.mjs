959351a274a43a416180cf594007058a36d25774
Minjo Park <consistent.312@gmail.com>
2025-03-11 18:29:00 +0900

    chore: 이미지 remotePatterns 추가

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,12 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'ddragon.leagueoflegends.com',
-        pathname: '/cdn/10.6.1/img/item/*',
+        pathname: '/cdn/img/champion/splash/*', // 챔피언 스플래시 이미지
+      },
+      {
+        protocol: 'https',
+        hostname: 'ddragon.leagueoflegends.com',
+        pathname: '/cdn/15.5.1/img/item/*',
       },
     ],
   },
