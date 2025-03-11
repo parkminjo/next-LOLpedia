728e2917ac053c08e5d8bfecf62ca696aec0e195
Minjo Park <consistent.312@gmail.com>
2025-03-11 14:48:14 +0900

    design: 기본 레이아웃 추가

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { Text } from '@/components/ui/Text';
 
 const Items = () => {
-  return <div>Items</div>;
+  return (
+    <div className="container mx-auto mt-10 flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <Text variant="h1">아이템 목록</Text>
+        <Text variant="h3" className="text-gray-500">
+          LOL에서 사용할 수 있는 아이템을 구경해보세요.
+        </Text>
+      </div>
+      <div className="grid grid-cols-6 gap-8"></div>
+    </div>
+  );
 };
 
 export default Items;
--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { Text } from '@/components/ui/Text';
 
 const Rotation = () => {
-  return <div>Rotation</div>;
+  return (
+    <div className="container mx-auto mt-10 flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <Text variant="h1">로테이션</Text>
+        <Text variant="h3" className="text-gray-500">
+          오늘 LOL에서 무료로 이용할 수 있는 챔피언을 확인해보세요.
+        </Text>
+      </div>
+      <div className="grid grid-cols-4 gap-8"></div>
+    </div>
+  );
 };
 
 export default Rotation;
