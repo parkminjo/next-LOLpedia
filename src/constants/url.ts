2c3e727f725a7b5c8c3f418aff4315601bc63759
Minjo Park <consistent.312@gmail.com>
2025-03-11 15:04:09 +0900

    chore: item 관련 url 추가

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -3,4 +3,6 @@ export const URL: Record<string, string> = {
   CHAMPIONS_DATA_URL: process.env.NEXT_RIOT_CHAMPIONS_URL || '',
   CHAMPION_IMG_URL:
     'https://ddragon.leagueoflegends.com/cdn/img/champion/loading',
+  ITEMS_DATA_URL: process.env.NEXT_RIOT_ITEMS_URL || '',
+  ITEMS_IMG_URL: 'https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item',
 };
