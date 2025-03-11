86b9688bd412e9e95b5703063e0a434477a42f25
Minjo Park <consistent.312@gmail.com>
2025-03-11 17:46:25 +0900

    feat: 챔피언 디테일로 이동하는 로직 추가

--- src/components/champions/ChampionCard.tsx
+++ src/components/champions/ChampionCard.tsx
@@ -2,6 +2,8 @@ import { URL } from '@/constants/url';
 import Champion from '@/types/Champion';
 import Image from 'next/image';
 import { Text } from '../ui/Text';
+import Link from 'next/link';
+import { PATH } from '@/constants/routerPath';
 
 interface ChampionCardProps {
   champion: Champion;
@@ -9,19 +11,21 @@ interface ChampionCardProps {
 
 const ChampionCard = ({ champion }: ChampionCardProps) => {
   return (
-    <div className="rounded-xl border-2 border-solid p-4 flex flex-col gap-3">
-      <div>
-        <Text variant="h2">{champion.name}</Text>
-        <Text className="text-gray-500">{champion.title}</Text>
+    <Link href={`${PATH.CHAMPIONS}/${champion.id}`}>
+      <div className="rounded-xl border-2 border-solid p-4 flex flex-col gap-3">
+        <div>
+          <Text variant="h2">{champion.name}</Text>
+          <Text className="text-gray-500">{champion.title}</Text>
+        </div>
+        <Image
+          src={`${URL.CHAMPION_IMG_URL}/${champion.id}_0.jpg`}
+          width={50}
+          height={50}
+          alt={champion.name}
+          className="w-screen max-h-[400px] object-cover"
+        />
       </div>
-      <Image
-        src={`${URL.CHAMPION_IMG_URL}/${champion.id}_0.jpg`}
-        width={50}
-        height={50}
-        alt={champion.name}
-        className="w-screen max-h-[400px] object-cover"
-      />
-    </div>
+    </Link>
   );
 };
 
