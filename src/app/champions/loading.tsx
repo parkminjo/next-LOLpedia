3050a962c53a22db70de458be3b51f102a6a0525
Minjo Park <consistent.312@gmail.com>
2025-03-11 16:24:18 +0900

    refactor: array 길이 상수로 관리

--- src/app/champions/loading.tsx
+++ src/app/champions/loading.tsx
@@ -1,10 +1,11 @@
 import SkeletonUI from '@/components/ui/SkeletonUI';
+import { ARRAY_LENGTH } from '@/constants/number';
 
 const Loading = () => {
   return (
     <div className="container mx-auto mt-10 flex flex-col gap-8">
       <div className="grid grid-cols-4 gap-8">
-        {Array.from({ length: 4 }).map((_, i) => {
+        {Array.from({ length: ARRAY_LENGTH.FOUR }).map((_, i) => {
           return (
             <div className="rounded-xl border-2 border-solid p-4 flex flex-col gap-3">
               <SkeletonUI />
--- src/constants/number.ts
+++ src/constants/number.ts
@@ -1,1 +1,6 @@
 export const REVALIDATE_TIME_24_HOURS = 86400;
+
+export const ARRAY_LENGTH = {
+  FOUR: 4,
+  SIX: 6,
+};
