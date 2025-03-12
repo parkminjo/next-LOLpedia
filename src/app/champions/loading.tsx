b7b54c43f7479a52fd0442378fd7d5c19e6e12da
Minjo Park <consistent.312@gmail.com>
2025-03-12 16:01:00 +0900

    refactor: ChampionsLoading 컴포넌트 제작

--- src/app/champions/loading.tsx
+++ src/app/champions/loading.tsx
@@ -1,25 +1,7 @@
-import SkeletonUI from '@/components/ui/SkeletonUI';
-import { ARRAY_LENGTH } from '@/constants/number';
+import ChampionsLoading from '@/components/ui/ChampionsLoading';
 
-const Loading = () => {
-  return (
-    <div className="container mx-auto mt-10 flex flex-col gap-8">
-      <div className="grid grid-cols-4 gap-8">
-        {Array.from({ length: ARRAY_LENGTH.FOUR }).map((item, i) => {
-          return (
-            <div
-              key={i}
-              className="rounded-xl border-2 border-solid p-4 flex flex-col gap-3"
-            >
-              <SkeletonUI />
-              <SkeletonUI />
-              <SkeletonUI height="large" />
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+export const Loading = () => {
+  return <ChampionsLoading />;
 };
 
 export default Loading;
---
+++ src/components/ui/ChampionsLoading.tsx
@@ -0,0 +1,25 @@
+import SkeletonUI from '@/components/ui/SkeletonUI';
+import { ARRAY_LENGTH } from '@/constants/number';
+
+export const ChampionsLoading = () => {
+  return (
+    <div className="container mx-auto mt-10 flex flex-col gap-8">
+      <div className="grid grid-cols-4 gap-8">
+        {Array.from({ length: ARRAY_LENGTH.FOUR }).map((item, i) => {
+          return (
+            <div
+              key={i}
+              className="rounded-xl border-2 border-solid p-4 flex flex-col gap-3"
+            >
+              <SkeletonUI />
+              <SkeletonUI />
+              <SkeletonUI height="large" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ChampionsLoading;
