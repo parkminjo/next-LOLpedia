45a6d045822a8da3169f1f45a401d8640c124723
Minjo Park <consistent.312@gmail.com>
2025-03-14 18:14:59 +0900

    feat: itemDetail로 이동할 수 있는 로직 추가

--- src/components/items/ItemCard.tsx
+++ src/components/items/ItemCard.tsx
@@ -2,6 +2,8 @@ import { URL } from '@/constants/url';
 import Image from 'next/image';
 import { Text } from '../ui/Text';
 import Item from '@/types/Item';
+import Link from 'next/link';
+import { PATH } from '@/constants/routerPath';
 
 interface ItemCardProps {
   item: Item;
@@ -9,18 +11,20 @@ interface ItemCardProps {
 
 const ItemCard = ({ item }: ItemCardProps) => {
   return (
-    <div className="flex flex-col gap-3 rounded-xl border-2 border-solid p-4">
-      <Text variant="h4" className="font-semibold">
-        {item.name}
-      </Text>
-      <Image
-        src={`${URL.ITEMS_IMG}/${item.image.full}`}
-        width={50}
-        height={50}
-        alt={item.name}
-        className="max-h-[400px] w-screen rounded-xl object-cover"
-      />
-    </div>
+    <Link href={`${PATH.ITEMS}/${item.name.replace(/(\s*)/g, '')}`}>
+      <div className="flex flex-col gap-3 rounded-xl border-2 border-solid p-4">
+        <Text variant="h4" className="font-semibold">
+          {item.name}
+        </Text>
+        <Image
+          src={`${URL.ITEMS_IMG}/${item.image.full}`}
+          width={50}
+          height={50}
+          alt={item.name}
+          className="max-h-[400px] w-screen rounded-xl object-cover"
+        />
+      </div>
+    </Link>
   );
 };
 
