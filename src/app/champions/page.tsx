cdf39738afe95ae490582dbbc0872f4244e6e824
Minjo Park <consistent.312@gmail.com>
2025-03-16 11:38:59 +0900

    design: margin 값 수정

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -8,7 +8,7 @@ const Champions = async () => {
   const championList = await fetchChampionList();
 
   return (
-    <div className="container my-10 flex flex-col gap-8 px-5 md:mx-auto">
+    <div className="container mx-auto my-10 flex flex-col gap-8 px-5">
       <div className="flex flex-col gap-2">
         <Text variant="h2">챔피언 목록</Text>
         <Text variant="h4" className="text-gray-500">
