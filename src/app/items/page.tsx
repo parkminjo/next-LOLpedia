007c4943943f69a77e602d2a0fd627d32e145a2c
Minjo Park <consistent.312@gmail.com>
2025-03-16 11:39:08 +0900

    design: 아이템 목록 페이지 반응형 디자인으로 수정

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -8,7 +8,7 @@ const Items = async () => {
   const itemList = await fetchItemList();
 
   return (
-    <div className="container mx-auto my-10 flex flex-col gap-8">
+    <div className="container mx-auto my-10 flex flex-col gap-8 px-5">
       <div className="flex flex-col gap-2">
         <Text variant="h2">아이템 목록</Text>
         <Text variant="h4" className="text-gray-500">
@@ -16,7 +16,7 @@ const Items = async () => {
         </Text>
       </div>
       <Suspense fallback={<Loading />}>
-        <div className="grid grid-cols-6 gap-8">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
           {itemList.map((item) => {
             return <ItemCard key={item.name} item={item} />;
           })}
