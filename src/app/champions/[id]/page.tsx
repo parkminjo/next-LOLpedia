e9032770d76b374470595734c3d5fca5ef7999f3
Minjo Park <consistent.312@gmail.com>
2025-03-17 22:32:02 +0900

    design: 챔피언 디테일 페이지 반응형 디자인 적용

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -27,7 +27,7 @@ const ChampionDetail = async ({ params }: ParamsProps) => {
   }
 
   return (
-    <div className="relative">
+    <div className="relative w-full">
       <Image
         src={`${URL.CHAMPION_IMG}/${championId}_0.jpg`}
         width={500}
@@ -37,14 +37,17 @@ const ChampionDetail = async ({ params }: ParamsProps) => {
         className="relative h-screen w-screen object-cover"
       />
       <div className="absolute inset-0 bg-black/50"></div>
-      <div className="absolute left-20 top-[40%] flex flex-col gap-4">
-        <Text variant="h1" className="text-gold">
+      <div className="absolute top-[30%] flex flex-col gap-4 px-8 md:left-20">
+        <Text variant="h2" className="text-gold md:text-5xl">
           {championData.name}
         </Text>
-        <Text variant="h3" className="text-gray-200">
+        <Text
+          variant="h4"
+          className="text-gray-200 md:text-2xl md:font-semibold"
+        >
           {championData.title}
         </Text>
-        <Text variant="h4" className="max-w-[600px] text-gray-200">
+        <Text className="max-w-[600px] text-gray-200 md:text-xl">
           {championData.lore}
         </Text>
       </div>
